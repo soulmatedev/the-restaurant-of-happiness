0e9fd3aba66e4dd3d4d33fe7548181cdec23cebc
soulmatedev <khokhlov.dev@gmail.com>
2024-06-21 18:39:40 +0500

    Добавьте файлы проекта.

---
+++ RestarauntH/GUI/PageController.cs
@@ -0,0 +1,27 @@
+﻿using RestarauntH.GUI.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestarauntH.GUI
+{
+    internal class PageController
+    {
+        private static MenuWindow menuWindow;
+        public static MenuWindow MenuWindow
+
+        {
+            get
+            {
+                if (menuWindow == null)
+                {
+                    menuWindow = new MenuWindow();
+                }
+                return menuWindow;
+            }
+        }
+    }
+}
