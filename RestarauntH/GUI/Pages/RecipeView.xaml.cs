3a5988b4abe85be3de4f44b7dec0d5c758705059
soulmatedev <khokhlov.dev@gmail.com>
2024-06-22 00:26:04 +0500

    Добавляет перенос ингредиентов

--- RestarauntH/GUI/Pages/RecipeView.xaml.cs
+++ RestarauntH/GUI/Pages/RecipeView.xaml.cs
@@ -1,28 +1,21 @@
 ﻿using Database;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RestarauntH.GUI.Pages
 {
     public partial class RecipeView : Page, INotifyPropertyChanged
     {
         private readonly RestarauntHEntities database;
-        public ObservableCollection<Ingredient> Ingredients { get; set; }
         private Ingredient _selectedIngredient;
+        private Ingredient _selectedRecipeIngredient;
+
+        public ObservableCollection<Ingredient> Ingredients { get; set; }
+        public ObservableCollection<Ingredient> RecipeIngredients { get; set; }
+
         public Ingredient SelectedIngredient
         {
             get => _selectedIngredient;
@@ -33,12 +26,45 @@ namespace RestarauntH.GUI.Pages
             }
         }
 
+        public Ingredient SelectedRecipeIngredient
+        {
+            get => _selectedRecipeIngredient;
+            set
+            {
+                _selectedRecipeIngredient = value;
+                OnPropertyChanged(nameof(SelectedRecipeIngredient));
+            }
+        }
+
         public RecipeView(RestarauntHEntities entities)
         {
             InitializeComponent();
             database = entities;
+
             Ingredients = new ObservableCollection<Ingredient>(database.Ingredients.ToList());
+            RecipeIngredients = new ObservableCollection<Ingredient>();
+
             DataContext = this;
+        }
+
+        private void MoveToRecipe_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (SelectedIngredient != null)
+            {
+                RecipeIngredients.Add(SelectedIngredient);
+                Ingredients.Remove(SelectedIngredient);
+                SelectedIngredient = null;
+            }
+        }
+
+        private void MoveToIngredients_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (SelectedRecipeIngredient != null)
+            {
+                Ingredients.Add(SelectedRecipeIngredient);
+                RecipeIngredients.Remove(SelectedRecipeIngredient);
+                SelectedRecipeIngredient = null;
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
